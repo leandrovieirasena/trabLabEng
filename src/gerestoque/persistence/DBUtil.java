a69ebdd70090b2b59e88dc9ee6f141871c49a918
MateusAndrade <mateus.andrade47@outlook.com>
2016-09-20 11:48:37 -0300

    Estrutura DAO criada, erro de SQL

--- src/gerestoque/persistence/DBUtil.java
+++ src/gerestoque/persistence/DBUtil.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-	private static final String URL = "jdbc:mysql://localhost:3535/lojas?autoReconnect=true";
+	private static final String URL = "jdbc:mysql://localhost:3535/teste";
 	private static final String USER="root";
-	private static final String PASS="root";
+	private static final String PASS="manager";
 	private static final String DRIVER="com.mysql.jdbc.Driver";
 	
 	private static DBUtil dbUtil = null;
